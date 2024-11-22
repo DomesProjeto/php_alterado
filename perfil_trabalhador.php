de911d839e60cd2129562c9ab551c35ef558bb55
Domes <domesprojeto@gmail.com>
2024-11-22 11:07:56 -0300

    Add files via upload

--- perfil_trabalhador.php
+++ perfil_trabalhador.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <!-- Configura o comportamento para o modo de compatibilidade do IE -->
@@ -17,40 +18,43 @@
     <!-- Atalho para o ícone do site -->
     <link rel="shortcut icon" href="img/tela_inicial/logo.png" type="image/x-icon">
 </head>
+
 <body>
     <header>
-      <!-- Navegação principal -->
-      <nav id="navbar">
-        <!-- Logo da navegação que redireciona para a página do menu -->
-        <a href="menu.php"><img src="img/menu/logo.png" alt="Descrição da imagem" id="nav_logo" ></a> 
-        <!-- Botão para editar o perfil -->
-        <button class="btn-default">
-          <a href="editar_trabalhador.php">Editar</a>
-        </button>
-      </nav>
+        <!-- Navegação principal -->
+        <nav id="navbar">
+            <!-- Logo da navegação que redireciona para a página do menu -->
+            <a href="menu.php"><img src="img/menu/logo.png" alt="Logo" id="nav_logo"></a>
+            <!-- Botão para editar o perfil -->
+            <button class="btn-default">
+                <a href="editar_trabalhador.php">Editar</a>
+            </button>
+        </nav>
     </header>
+
     <div class="wrapper">
         <!-- Cartão do usuário -->
         <div class="user-card">
             <!-- Imagem do usuário -->
             <div class="user-card-img">
-              <img src="img/perfilexpositivo/avatar2.png" alt="">
+                <img src="img/perfilexpositivo/avatar2.png" alt="Foto de Carlos Nogueira">
             </div>
             <!-- Informações do usuário -->
             <div class="user-card-info">
-              <!-- Nome do usuário -->
-              <h2>Carlos Nogueira</h2>
-              <span></span>
-              <!-- Email do usuário -->
-              <p><span>Email:</span>nogueiracarlos@gmail.com</p>
-              <!-- Número de celular do usuário -->
-              <p><span>Celular:</span>(xx) xxxxx-xxxx</p>
-              <!-- Localização do usuário -->
-              <p><span>Localização:</span> Serra, Espirito Santo</p>
-              <!-- Sobre mim do usuário -->
-              <p><span>Sobre mim:</span> Trabalho fora de casa em tempo integral, por isso preciso que o aplicativo me ajude a encontrar pessoas para fazerem tarefas domesticas.</p>
+                <!-- Nome do usuário -->
+                <h2>Carlos Nogueira</h2>
+                <span></span>
+                <!-- Email do usuário -->
+                <p><span>Email:</span> nogueiracarlos@gmail.com</p>
+                <!-- Número de celular do usuário -->
+                <p><span>Celular:</span> (xx) xxxxx-xxxx</p>
+                <!-- Localização do usuário -->
+                <p><span>Localização:</span> Serra, Espirito Santo</p>
+                <!-- Sobre mim do usuário -->
+                <p><span>Sobre mim:</span> Trabalho fora de casa em tempo integral, por isso preciso que o aplicativo me ajude a encontrar pessoas para fazerem tarefas domésticas.</p>
             </div>
         </div>
     </div>
 </body>
+
 </html>
--- telainicial.php
+++ telainicial.php
@@ -2,22 +2,20 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <!-- Configurando a viewport para a responsividade -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Link para o arquivo CSS do Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Link para o arquivo CSS personalizado -->
     <link rel="stylesheet" href="css/tela_inicial/styles.css">
     <!-- Favicon da página -->
-    <link rel="shortcut icon" href="img/tela_inicial/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="img/menu/logo.png" type="image/x-icon">
     <!-- Link para o arquivo JavaScript do jQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Link para o arquivo JavaScript do ScrollReveal -->
     <script src="https://unpkg.com/scrollreveal"></script>
     <!-- Link para o arquivo JavaScript personalizado -->
     <script src="js/tela_inicial.js"></script>
-    <link rel="stylesheet" href="css/login.css"> <!-- Link para a folha de estilo local -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/> <!-- Link para o ícone FontAwesome -->
+    <!-- Link para o arquivo Popup -->
     <script src="js/popup.js"></script>
     <!-- Título da página -->
     <title>Domes</title>
@@ -28,51 +26,25 @@
         <!-- Barra de navegação -->
         <nav id="navbar">
             <!-- Logo do site -->
-            <img src="img/tela_inicial/logo.png" alt="Descrição da imagem" id="nav_logo"> 
+            <img src="img/menu/logo.png" alt="Descrição da imagem" id="nav_logo">
             <!-- Lista de navegação -->
             <ul id="nav_list">
-                <!-- Item do menu com link para a seção Início -->
-                <li class="nav-item active">
-                    <a href="#home">Início</a>
-                </li>
-                <!-- Item do menu com link para a seção Serviços -->
-                <li class="nav-item">
-                    <a href="#menu">Serviços</a>
-                </li>
-                <!-- Item do menu com link para a seção Avaliações -->
-                <li class="nav-item">
-                    <a href="#testimonials">Avaliações</a>
-                </li>
+                <li class="nav-item active"><a href="#home">Início</a></li>
+                <li class="nav-item"><a href="#menu">Serviços</a></li>
+                <li class="nav-item"><a href="#testimonials">Avaliações</a></li>
             </ul>
-            <!-- Botão para cadastro -->
-            <button class="btn-default">
-                <a href="cadastro_trabalhador.php">Cadastrar como Trabalhador</a>
-            </button>
-            <!-- Botão para login -->
-            <button class="btn-default">
-                <a href="login.php">Entrar</a>
-            </button>
+            <!-- Botões de Cadastro e Login -->
+            <button class="btn-default"><a href="cadastro_trabalhador.php">Cadastrar como Trabalhador</a></button>
+            <button class="btn-default"><a href="login.php">Entrar</a></button>
             <!-- Botão do menu móvel -->
-            <button id="mobile_btn">
-                <i class="fa-solid fa-bars"></i>
-            </button>
+            <button id="mobile_btn"><i class="fa-solid fa-bars"></i></button>
         </nav>
         <!-- Menu móvel -->
         <div id="mobile_menu">
-            <!-- Lista de navegação para o menu móvel -->
             <ul id="mobile_nav_list">
-                <!-- Item do menu com link para a seção Início -->
-                <li class="nav-item">
-                    <a href="#home">Início</a>
-                </li>
-                <!-- Item do menu com link para a seção Serviços -->
-                <li class="nav-item">
-                    <a href="#menu">Serviços</a>
-                </li>
-                <!-- Item do menu com link para a seção Avaliações -->
-                <li class="nav-item">
-                    <a href="#testimonials">Avaliações</a>
-                </li>
+                <li class="nav-item"><a href="#home">Início</a></li>
+                <li class="nav-item"><a href="#menu">Serviços</a></li>
+                <li class="nav-item"><a href="#testimonials">Avaliações</a></li>
             </ul>
         </div>
     </header>
@@ -81,176 +53,174 @@
     <main id="content">
         <!-- Seção Início -->
         <section id="home">
-            <!-- Conteúdo da seção -->
             <div class="shape"></div>
             <div id="cta">
-                <h1 class="title">
-                    Encontre Oportunidades de Trabalho e Conquiste sua Independência
-                    <span>Cadastre-se Agora!</span>
-                </h1>
-                <p class="description">
-                    Ofereça seus serviços para pessoas que precisam da sua ajuda.
-                </p>
-                <strong><p class="description">
-                    Áreas mais procuradas por trabalhadores:
-                </p></strong>
+                <h1 class="title">Encontre Oportunidades de Trabalho e Conquiste sua Independência <span>Cadastre-se Agora!</span></h1>
+                <p class="description">Ofereça seus serviços para pessoas que precisam da sua ajuda.</p>
+                <strong><p class="description">Áreas mais procuradas por trabalhadores:</p></strong>
                 <div id="cta_buttons">
-                    <a href="cadastro_trabalhador.php#diarista" class="btn-default">
-                        Diarista
-                    </a>
-                    <a href="cadastro_trabalhador.php#pintura" class="btn-default">
-                        Pintura
-                    </a>
-                    <a href="cadastro_trabalhador.php#eletrica" class="btn-default">
-                        Elétrica
-                    </a>
+                    <a href="cadastro_trabalhador.php#diarista" class="btn-default">Diarista</a>
+                    <a href="cadastro_trabalhador.php#pintura" class="btn-default">Pintura</a>
+                    <a href="cadastro_trabalhador.php#eletrica" class="btn-default">Elétrica</a>
                 </div>
                 <div class="social-media-buttons">
-                    <a href="">
-                        <i class="fa-brands fa-whatsapp"></i>
-                    </a>
-                    <a href="">
-                        <i class="fa-brands fa-instagram"></i>
-                    </a>
-                    <a href="">
-                        <i class="fa-brands fa-facebook"></i>
-                    </a>
+                    <a href=""><i class="fa-brands fa-whatsapp"></i></a>
+                    <a href=""><i class="fa-brands fa-instagram"></i></a>
+                    <a href=""><i class="fa-brands fa-facebook"></i></a>
                 </div>
             </div>
-            <div id="banner">
-                <img src="img/tela_inicial/home.png" alt="">
-            </div>
+            <div id="banner"><img src="img/menu/home.png" alt=""></div>
         </section>
-          <!-- Seção Serviços -->
-          <section id="menu">
-            <!-- Título da seção -->
+
+        <!-- Seção Serviços -->
+        <section id="menu">
             <h2 class="section-title">Áreas de Trabalho</h2>
-            <!-- Subtítulo da seção -->
             <h3 class="section-subtitle">Inscreva-se para oferecer serviços em</h3>
-            <!-- Div para os serviços mais buscados -->
             <div id="buscados">
-                <!-- Cada serviço com suas informações -->
                 <div class="buscas">
-                    <img src="img/tela_inicial/serviço1.png" class="buscas-image" alt="">
-                    <h3 class="buscas-title">
-                        Serviços de Elétrica
-                    </h3>
-                    <span class="buscas-description">
-                        Ofereça serviços de instalação e manutenção elétrica.
-                    </span>
+                    <img src="img/menu/serviço1.png" class="buscas-image" alt="">
+                    <h3 class="buscas-title">Serviços de Elétrica</h3>
+                    <span class="buscas-description">Ofereça serviços de instalação e manutenção elétrica.</span>
                 </div>
                 <div class="buscas">
-                    <img src="img/tela_inicial/serviço2.png" class="buscas-image" alt="">
-                    <h3 class="buscas-title">
-                        Serviços de Mudanças
-                    </h3>
-                    <span class="buscas-description">
-                        Ajudar com montagem, desmontagem e transporte de móveis.
-                    </span>
-                    
+                    <img src="img/menu/serviço2.png" class="buscas-image" alt="">
+                    <h3 class="buscas-title">Serviços de Mudanças</h3>
+                    <span class="buscas-description">Ajudar com montagem, desmontagem e transporte de móveis.</span>
                 </div>
                 <div class="buscas">
-                    <img src="img/tela_inicial/serviço3.png" class="buscas-image" alt="">
-                    <h3 class="buscas-title">
-                        Serviços de Pintura
-                    </h3>
-                    <span class="buscas-description">
-                        Pintura de móveis e espaços residenciais.
-                    </span>
-                    
+                    <img src="img/menu/serviço3.png" class="buscas-image" alt="">
+                    <h3 class="buscas-title">Serviços de Pintura</h3>
+                    <span class="buscas-description">Pintura de móveis e espaços residenciais.</span>
                 </div>
                 <div class="buscas">
                     <img src="img/menu/serviço4.png" class="buscas-image" alt="">
-                    <h3 class="buscas-title">
-                        Serviços de Diarista
-                    </h3>
-                    <span class="buscas-description">
-                        Limpeza residencial e organização de ambientes.
-                    </span>
-                    
+                    <h3 class="buscas-title">Serviços de Diarista</h3>
+                    <span class="buscas-description">Limpeza residencial e organização de ambientes.</span>
                 </div>
             </div>
         </section>
 
         <!-- Seção Avaliações -->
         <section id="testimonials">
-            <!-- Imagem de comentários -->
-            <img src="img/tela_inicial/comentários.png" id="testimonials_comentários" alt="">
+            <img src="img/menu/comentários.png" id="testimonials_comentários" alt="">
             <div id="testimonials_content">
-                <!-- Título da seção -->
-                <h2 class="section-title">
-                    Feedbacks dos Clientes da Plataforma
-                </h2>
-                <!-- Subtítulo da seção -->
-                <h3 class="section-subtitle">
-                    Retorno dos trabalhos
-                </h3>
-                <!-- Div para os feedbacks -->
+                <h2 class="section-title">Feedbacks dos Clientes da Plataforma</h2>
+                <h3 class="section-subtitle">Retorno dos trabalhos</h3>
                 <div id="feedbacks">
-                    <!-- Cada feedback com suas informações -->
                     <div class="feedback">
-                        <img src="img/tela_inicial/icon1.png" class="feedback-avatar"alt="">
+                        <img src="img/menu/icon1.png" class="feedback-avatar" alt="">
                         <div class="feedback-content">
-                            <p>
-                                Carlos Almeida
-                                <span>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                </span>
-                            </p>
-                            <p>
-                                "Achei aqui no app um ótimo eletricista, ele fez a rede elétrica da nova casa da minha Mãe. Contratei um pintor agora e gostei muito da facilidade do aplicativo."
-                            </p>
+                            <p>Carlos Almeida <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></span></p>
+                            <p>"Achei aqui no app um ótimo eletricista, ele fez a rede elétrica da nova casa da minha Mãe. Contratei um pintor agora e gostei muito da facilidade do aplicativo."</p>
                         </div>
                     </div>
-                    <div id="feedbacks">
-                        <div class="feedback">
-                            <img src="img/tela_inicial/icon2.jpg" class="feedback-avatar"alt="">
-                            <div class="feedback-content">
-                                <p>
-                                    Bianca Gomes
-                                    <span>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                        <i class="fa-solid fa-star"></i>
-                                    </span>
-                                </p>
-                                <p>
-                                    "Cheguei na cidade recentemente e esse app tem salvado a minha vida. Já contratei várias vezes e recomendo principalmente o serviço de limpeza deles"
-                                </p>
-                            </div>
+                    <div class="feedback">
+                        <img src="img/menu/icon2.jpg" class="feedback-avatar" alt="">
+                        <div class="feedback-content">
+                            <p>Bianca Gomes <span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></span></p>
+                            <p>"Cheguei na cidade recentemente e esse app tem salvado a minha vida. Já contratei várias vezes e recomendo principalmente o serviço de limpeza deles."</p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
     </main>
-    </main>
 
     <!-- Rodapé da página -->
     <footer>
-        <img src="img/tela_inicial/wave.svg" alt="">
+        <img src="img/menu/wave.svg" alt="">
         <div id="footer_items">
-            <span id="copyright" position:absolute>
-                &copy; 2024 DOMES
-            </span>
+            <span id="copyright">&copy; 2024 DOMES</span>
             <div class="social-media-buttons">
-                <a href="">
-                    <i class="fa-brands fa-whatsapp"></i>
-                </a>
-                <a href="">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-                <a href="">
-                    <i class="fa-brands fa-facebook"></i>
-                </a>
+                <a href=""><i class="fa-brands fa-whatsapp"></i></a>
+                <a href=""><i class="fa-brands fa-instagram"></i></a>
+                <a href=""><i class="fa-brands fa-facebook"></i></a>
             </div>
         </div>
     </footer>
 </body>
+<div id="loginPopup" class="popup">
+
+    <div class="popup-content">
+
+        <main id="login-container"> <!-- Container principal para o formulário de login -->
+
+            <form id="login_form"> <!-- Formulário de login -->
+
+                <div id="form_header"> <!-- Cabeçalho do formulário -->
+
+                    <h1>Faça o seu Login</h1>
+
+                </div>
+
+    
+
+                <div id="inputs"> <!-- Grupo de campos de entrada -->
+
+                    <div class="input-box"> <!-- Caixa de entrada para o e-mail -->
+
+                        <label for="email">
+
+                            <h2>E-mail:</h2> <!-- Rótulo para o campo de e-mail -->
+
+                            <div class="input-field"> <!-- Campo de entrada para o e-mail -->
+
+                                <i class="fa-solid fa-envelope"></i> <!-- Ícone de envelope -->
+
+                                <input type="email" id="email" name="email"> <!-- Campo de texto para o e-mail -->
+
+                            </div>
+
+                        </label>
+
+                    </div>
+
+                    <div class="input-box"> <!-- Caixa de entrada para a senha -->
+
+                        <label for="password">
+
+                            <h2>Senha:</h2> <!-- Rótulo para o campo de senha -->
+
+                            <div class="input-field"> <!-- Campo de entrada para a senha -->
+
+                                <i class="fa-solid fa-user"></i> <!-- Ícone de usuário -->
+
+                                <input type="password" id="password" name="password"> <!-- Campo de texto para a senha -->
+
+                            </div>
+
+                        </label>
+
+                        <div id="forgot-password"> <!-- Link para recuperar senha -->
+
+                            <a href="#">
+
+                                <h3>Esqueci minha Senha</h3>
+
+                            </a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                
+
+                <button type="submit" id="login-button"> <!-- Botão de login -->
+
+                    <a href="menu.html">Entrar</a> <!-- Link para a página de menu -->
+
+                </button>          
+
+            </form>
+
+        </main>
+
+    </div>
+
+</div>
+
+
+
 </html>
